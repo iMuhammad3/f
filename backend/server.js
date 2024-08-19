dce69a988d990f94f57ef9a75cf55e776dbe996e
Muhammad <auwalmuhd365@gmail.com>
2024-08-19 20:45:54 +0100

    add json limit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({limit: "8mb"}));
 app.use(express.urlencoded());
 app.use(cookieParser());
 
