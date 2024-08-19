4449d4ef522549a6c7b736f26b1dd09fe147a2da
Muhammad <auwalmuhd365@gmail.com>
2024-08-19 15:29:40 +0100

    install cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
-import connectDB from './db/connectDB.js';
+import connectDB from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(process.env.PORT, () => {
-  connectDB();
-})
+    connectDB();
+});
